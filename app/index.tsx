33e2f1dbd17cad1d87bbeb6a0486b6c1afe78c7e
Nick <nick.ferkalek@gmail.com>
2025-11-30 10:51:18 +0100

    CHORE: Code structure changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,10 +1,13 @@
 
-import { Image, ImageSourcePropType, Pressable, StyleSheet, Text, View, BackHandler, Alert } from 'react-native'; import FrogImg from '@/components/FrogImg';
+import { Image, ImageSourcePropType, Pressable, StyleSheet, Text, View, BackHandler, Alert } from 'react-native'; 
+import FrogImg from '@/components/FrogImg';
 import { Int32 } from 'react-native/Libraries/Types/CodegenTypes';
 import { useEffect, useRef, useState } from 'react';
 import { frogData } from '@/components/frogData';
 import { Frog } from '@/components/frogs';
 import { Audio } from 'expo-av';
+
+import { styles } from './style';
 
 //sound of the clickedFog
 const playFrogSound = async () => {
@@ -13,6 +16,13 @@ const playFrogSound = async () => {
     );
     await sound.playAsync();
 };
+
+
+
+type WinScreenProps = {
+    points: number;
+};
+
 
 export default function HomeScreen() {
     const frogImages: ImageSourcePropType[] = [
@@ -37,18 +47,20 @@ export default function HomeScreen() {
     const pauseGame = () => (pauseRef.current = true);
     const resume = () => (pauseRef.current = false);
 
-
-    function clickedFrog(id: number) {
-        playFrogSound();
-        setFrogs(prev => prev.filter(f => f.id !== id));
-        setScore(prev => prev + 1);
-    }
+    const toStartScreen = () => {
+        setScore(0);
+        setFrogs([]);
+        setGameOver(false);
+        setIsPlaying(false);
+        showStartScreen(true);
+        pauseGame();
+    };
 
 
     //frogs spawn interval
     useEffect(() => {
-        if (score >= requireFrogs*3 + 7) { setGameOver(true); }
-    }, [score]);
+        if (score >= requireFrogs * 3 + 7) { setGameOver(true); }
+    }, [score, setGameOver, requireFrogs]);
 
 
 
@@ -84,18 +96,17 @@ export default function HomeScreen() {
         }, 1000);
 
         return () => clearInterval(interval);
-    }, [isPlaying]);
+    }, [isPlaying, setFrogs]);
 
 
 
-    const toStartScreen = () => {
-        setScore(0);
-        setFrogs([]);
-        setGameOver(false);
-        setIsPlaying(false);
-        showStartScreen(true);
-        pauseGame();
-    };
+
+    function clickedFrog(id: number) {
+        playFrogSound();
+        setFrogs(prev => prev.filter(f => f.id !== id));
+        setScore(prev => prev + 1);
+    }
+
 
 
     //difficulty settings
@@ -127,30 +138,6 @@ export default function HomeScreen() {
 
 
 
-
-    //main return function
-    return (
-        <>
-            <Image source={require('@/assets/images/fullbackground.png')} style={styles.backGorundImage} />
-            <Image source={require('@/assets/images/fullfrontground.png')} style={styles.backGorundImage} />
-            <Text style={styles.score}>x{score}</Text>
-
-            {startScreen && <StartScreen onStart={() => { setIsPlaying(true); showStartScreen(false); }} />}
-            {gameOver && <WinScreen points={score} />}
-
-            {frogs.map(frog => (
-                <FrogImg key={frog.id} frog={frog} onPress={clickedFrog} />
-            ))}
-        </>
-    );
-
-
-
-
-    //additional displays
-    type WinScreenProps = {
-        points: number;
-    };
     function WinScreen({ points }: WinScreenProps) {
         useEffect(() => {
             pauseGame();
@@ -179,153 +166,31 @@ export default function HomeScreen() {
                 <Text style={[styles.sText, styles.mainText]}>Find the frog!</Text>
                 <Text style={styles.sText}>Select the difficulty and play:</Text>
                 <Text style={{ flex: 1 }}></Text><Pressable onPress={() => { onStart(); resume(); }} style={styles.sButton} ><Text style={{ textAlign: 'center', lineHeight: 40, fontSize: 20 }}>Play</Text></Pressable>
-                <Text style={{ flex: 1 }}></Text>< Pressable onPress={changeDifficulty} style={[styles.sButton, { backgroundColor: difficultyStyles[0] }]}><Text style={{ textAlign: 'center', lineHeight: 40, fontSize: 20, color: difficultyStyles[1], fontFamily: difficultyStyles[3]}}>{difficultyStyles[2]}</Text></Pressable>
+                <Text style={{ flex: 1 }}></Text>< Pressable onPress={changeDifficulty} style={[styles.sButton, { backgroundColor: difficultyStyles[0] }]}><Text style={{ textAlign: 'center', lineHeight: 40, fontSize: 20, color: difficultyStyles[1], fontFamily: difficultyStyles[3] }}>{difficultyStyles[2]}</Text></Pressable>
                 <Text style={{ flex: 1 }}></Text>< Pressable onPress={() => BackHandler.exitApp()} style={styles.sButton}><Text style={{ textAlign: 'center', lineHeight: 40, fontSize: 20 }}>Exit game</Text></Pressable>
                 <Text style={{ flex: 1 }}></Text>
             </View>
         )
     }
+
+
+
+
+    return (
+        <>
+            <Image source={require('@/assets/images/fullbackground.png')} style={styles.backGorundImage} />
+            <Image source={require('@/assets/images/fullfrontground.png')} style={styles.backGorundImage} />
+            <Text style={styles.score}>x{score}</Text>
+
+            {startScreen && <StartScreen onStart={() => { setIsPlaying(true); showStartScreen(false); }} />}
+            {gameOver && <WinScreen points={score} />}
+
+            {frogs.map(frog => (
+                <FrogImg key={frog.id} frog={frog} onPress={clickedFrog} />
+            ))}
+        </>
+    );
+
+
 }
 
-
-
-
-
-
-export const styles = StyleSheet.create({
-
-    backGorundImage: {
-        position: 'absolute',
-        top: 0,
-        left: 0,
-        width: "100%",
-        height: "100%",
-        zIndex: -2,
-    },
-    frogImage: {
-        position: 'absolute',
-        top: "60%",
-        flex: 5,
-        height: 'auto',
-        width: 200,
-    },
-    score: {
-        marginBottom: 1,
-        position: 'absolute',
-        bottom: 0,
-        left: 0,
-        width: 70,
-        height: 40,
-        lineHeight: 37,
-        textAlign: 'center',
-        fontSize: 30,
-        borderStyle: 'solid',
-        borderWidth: 4,
-        borderColor: 'yellow',
-        color: 'yellow',
-        backgroundColor: '#98d40c93',
-        zIndex: 10,
-    },
-
-    //Win screen styles
-    mainWinWindow: {
-        position: 'absolute',
-        flexDirection: 'column',
-
-        width: 225,
-        height: 300,
-        left: 300,
-        top: 75,
-
-        backgroundColor: "#ebeae8ff",
-        color: "ghostwhite",
-
-        borderColor: "#80FF00",
-        borderWidth: 3,
-        borderStyle: "solid",
-        borderRadius: 20,
-
-        zIndex: 3,
-        padding: 10,
-    },
-
-    text: {
-        flex: 3,
-        color: "#003262",
-        width: "100%",
-        height: 10,
-        fontFamily: 'monospace',
-        fontSize: 18,
-        textAlign: 'center',
-        lineHeight: 20,
-    },
-
-    button: {
-        flex: 2,
-        zIndex: 4,
-        backgroundColor: '#FF7900',
-        color: '#242423',
-        borderColor: 'black',
-        borderStyle: 'solid',
-        borderWidth: 2,
-        borderRadius: 10,
-        width: "100%",
-        height: "15%",
-        padding: 5,
-    },
-
-
-
-
-    //start screen settings
-
-    mainStartWindow: {
-        position: 'absolute',
-        flexDirection: 'column',
-
-        width: 275,
-        height: 420,
-        left: "35%",
-        top: 5,
-
-        backgroundColor: "#ebeae8ff",
-        color: "ghostwhite",
-
-        borderColor: "#80FF00",
-        borderWidth: 3,
-        borderStyle: "solid",
-        borderRadius: 20,
-
-        zIndex: 3,
-        padding: 10,
-    },
-
-    sText: {
-        flex: 2,
-        color: "#003262",
-        width: "100%",
-        height: 10,
-        fontFamily: 'monospace',
-        fontSize: 18,
-        textAlign: 'center',
-        lineHeight: 17,
-    },
-    mainText: {
-        textTransform: 'uppercase',
-        fontWeight: 900,
-    },
-
-    sButton: {
-        flex: 2,
-        zIndex: 4,
-        backgroundColor: '#FF7900',
-        color: '#242423',
-        borderColor: 'black',
-        borderStyle: 'solid',
-        borderWidth: 2,
-        borderRadius: 10,
-        width: "100%",
-        height: "15%",
-        padding: 5,
-    }
-});
---
+++ app/style.ts
@@ -0,0 +1,139 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+
+    backGorundImage: {
+        position: 'absolute',
+        top: 0,
+        left: 0,
+        width: "100%",
+        height: "100%",
+        zIndex: -2,
+    },
+    frogImage: {
+        position: 'absolute',
+        top: "60%",
+        flex: 5,
+        height: 'auto',
+        width: 200,
+    },
+    score: {
+        marginBottom: 1,
+        position: 'absolute',
+        bottom: 0,
+        left: 0,
+        width: 70,
+        height: 40,
+        lineHeight: 37,
+        textAlign: 'center',
+        fontSize: 30,
+        borderStyle: 'solid',
+        borderWidth: 4,
+        borderColor: 'yellow',
+        color: 'yellow',
+        backgroundColor: '#98d40c93',
+        zIndex: 10,
+    },
+
+    //Win screen styles
+    mainWinWindow: {
+        position: 'absolute',
+        flexDirection: 'column',
+
+        width: 225,
+        height: 300,
+        left: 300,
+        top: 75,
+
+        backgroundColor: "#ebeae8ff",
+        color: "ghostwhite",
+
+        borderColor: "#80FF00",
+        borderWidth: 3,
+        borderStyle: "solid",
+        borderRadius: 20,
+
+        zIndex: 3,
+        padding: 10,
+    },
+
+    text: {
+        flex: 3,
+        color: "#003262",
+        width: "100%",
+        height: 10,
+        fontFamily: 'monospace',
+        fontSize: 18,
+        textAlign: 'center',
+        lineHeight: 20,
+    },
+
+    button: {
+        flex: 2,
+        zIndex: 4,
+        backgroundColor: '#FF7900',
+        color: '#242423',
+        borderColor: 'black',
+        borderStyle: 'solid',
+        borderWidth: 2,
+        borderRadius: 10,
+        width: "100%",
+        height: "15%",
+        padding: 5,
+    },
+
+
+
+
+    //start screen settings
+
+    mainStartWindow: {
+        position: 'absolute',
+        flexDirection: 'column',
+
+        width: 275,
+        height: 420,
+        left: "35%",
+        top: 5,
+
+        backgroundColor: "#ebeae8ff",
+        color: "ghostwhite",
+
+        borderColor: "#80FF00",
+        borderWidth: 3,
+        borderStyle: "solid",
+        borderRadius: 20,
+
+        zIndex: 3,
+        padding: 10,
+    },
+
+    sText: {
+        flex: 2,
+        color: "#003262",
+        width: "100%",
+        height: 10,
+        fontFamily: 'monospace',
+        fontSize: 18,
+        textAlign: 'center',
+        lineHeight: 17,
+    },
+    mainText: {
+        textTransform: 'uppercase',
+        fontWeight: 900,
+    },
+
+    sButton: {
+        flex: 2,
+        zIndex: 4,
+        backgroundColor: '#FF7900',
+        color: '#242423',
+        borderColor: 'black',
+        borderStyle: 'solid',
+        borderWidth: 2,
+        borderRadius: 10,
+        width: "100%",
+        height: "15%",
+        padding: 5,
+    }
+}); 
